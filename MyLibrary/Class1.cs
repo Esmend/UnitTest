6e79f2541a534104d1d6da427720a0415fc38f70
Андрей Чернышев <chernshev97@mail.ru>
2023-12-15 10:40:25 +0500

    Добавьте файлы проекта.

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace MyLibrary
+{
+    public class Mylibrary
+    {
+        public double Add(double x, double y)
+        {
+            return x + y  ;
+        }
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+
+        public double Pow(double x, double y)
+        {
+            double result = 1;
+            for (double i = 0; i < y; i++)
+            {
+                result *= x;
+            }
+            return result;
+        }
+
+        public double Sqrt(double x)
+        {
+            double res = 1;
+            res /= x;
+            return res;
+        }
+    }
+}
---
+++ MyLibraryTests/Class1Tests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            double x = 10;
+            double y = 20;
+            double expected = 30;//всё верно
+
+            Mylibrary m = new Mylibrary();
+            double actual = m.Add(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            double x = 20;
+            double y = 10;
+            double expected = 9;//допущена ошибка
+
+            Mylibrary m = new Mylibrary();
+            double actual = m.Subtract(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            double x = 7;
+            double y = 7;
+            double expected = 48;//допущена ошибка
+
+            Mylibrary m = new Mylibrary();
+            double actual = m.Multiply(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            double x = 14;
+            double y = 7;
+            double expected = 2;//всё верно
+
+            Mylibrary m = new Mylibrary();
+            double actual = m.Divide(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PowTest()
+        {
+            double x = 11;
+            double y = 2;
+            double expected = 121;//всё верно
+
+            Mylibrary m = new Mylibrary();
+            double actual = m.Pow(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
